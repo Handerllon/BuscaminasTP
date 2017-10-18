e4a1aac47f1f17472f3efa558bcedb03eebc594d
RamiroChogri <32467892+RamiroChogri@users.noreply.github.com>
2017-10-18 15:34:36 -0300

    Add files via upload

---
+++ Planilla.cpp
@@ -0,0 +1,33 @@
+/*
+ * Planilla.cpp
+ *
+ *  Created on: Oct 18, 2017
+ *      Author: lubuntu
+ */
+
+#include "Planilla.h"
+#include <string>
+
+
+Planilla::Planilla(int jugador) {
+	if(jugador<1){
+		 throw std::string("No puede existir un jugador menor al 1");
+	}
+	else{
+		this->NumeroJugador= jugador;
+		this->puntosTotales= 0;
+	}
+}
+
+int Planilla:: obtenerPuntaje(){
+	return this->puntosTotales;
+}
+
+void Planilla:: sumarPuntos(int puntosASumar){
+	this->puntosTotales+= puntosASumar;
+}
+
+Planilla::~Planilla() {
+
+}
+
---
+++ Planilla.h
@@ -0,0 +1,34 @@
+/*
+ * Planilla.h
+ *
+ *  Created on: Oct 18, 2017
+ *      Author: lubuntu
+ */
+
+#ifndef PLANILLA_H_
+#define PLANILLA_H_
+
+class Planilla {
+
+	private:
+
+			int NumeroJugador;
+			int puntosTotales;
+
+
+	public:
+
+	//Pre: Jugador debe ser mayor a 0.
+	//post: Crea una Planilla con los puntos del jugador iniciados en 0.
+	Planilla(int jugador);
+	//
+	//Post: Devuelve el puntaje actual del jugador.
+	int obtenerPuntaje();
+	//
+	//Post: Suma los puntos que gano o perdio en su turno.
+	void sumarPuntos(int puntosASumar);
+
+	virtual ~Planilla();
+};
+#endif /* PLANILLA_H_ */
+
