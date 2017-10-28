7e661ea057314dfde231e22698146d0d0ae1cf91
Handerllon <manuel121097@gmail.com>
2017-10-28 15:15:27 -0300

    Agregado jugador

--- Jugador.cpp
+++ Jugador.cpp
@@ -1,45 +1,68 @@
-/*
- * Jugador.cpp
- *
- *  Created on: Oct 18, 2017
- *      Author: lubuntu
- */
-
 #include "Jugador.h"
-#include <string>
+#include <iostream>
 
-Jugador::Jugador(int jugadorNumero) {
-	if(jugadorNumero < 1){
-		throw std:: string("jugadorNumero debe ser mayor a 0");
-	}
-	else{
-		this->numeroDeJugador= jugadorNumero;
-		this->sigueJugando= true;
-	}
+using namespace std;
+
+Jugador::Jugador(unsigned int identificador){
+
+    estaJugando=true;
+
+    identificadorJugador=identificador;
+
 }
 
-void Jugador:: Jugada(char accion){
-	if(accion == 'D' || accion == 'd'){
+Jugador::cambiarJugadorAPerdido(){
 
-	}
-	else if(accion == 'B' || accion == 'b'){
+    estaJugando=false;
 
-	}
-	else if(accion == 'R' || accion == 'r'){
-
-		}
-	else if(accion == 'Q' || accion == 'q'){
-
-		}
-	else if(accion == 'S' || accion == 's'){
-
-		}
-	else{
-		throw std:: string("Jugada invalida");
-	}
 }
 
-Jugador::~Jugador() {
-	// TODO Auto-generated destructor stub
+Jugador::getEstaJugando(){
+
+    return estaJugando;
+
 }
 
+Jugador::getIdentificador(){
+
+    return identificadorJugador;
+
+}
+
+Jugador::jugada(Tablero* tablero){
+
+    unsigned int filaElegida,columnaElegida,tipoDeJugada;
+
+    bool jugadaValida=false
+
+    cout<<"Por favor ingresar la fila elegida para jugar"<<endl;
+    cin>>filaElegida;
+
+    cout<<"Por favor ingresar la columna elegida para jugar"<<endl;
+    cin>>columnaElegida;
+
+    Casilla* casillaElegida= tablero->obtenerCasillero(filaElegida, columnaElegida);
+
+    while (not jugadaValida){
+
+        cout>>"Por favor ingresar el numero que corresponde a la jugada elegida"<<endl;
+        cout>>"(1) Descubrir casillero">>endl>>"(2) Colocar bandera">>endl>>"(3) Quitar bandera">>endl;
+        cin<<tipoDeJugada;
+
+        if (tipoDeJugada==1){
+            casillaElegida.descubrirCasillero;
+            jugadaValida=true;
+        }
+        else if (tipoDeJugada==2){
+            casillaElegida.colocarBandera;
+            jugadaValida=true;
+        }
+        else if (tipoDeJugada==3){
+            casillaElegida.quitarBandera;
+            jugadaValida=true;
+        }
+        else{
+            cout>>"El tipo de jugada ingresado no es valido, por favor ingresar nuevamente">>endl;
+        }
+    }
+}
--- Jugador.h
+++ Jugador.h
@@ -1,38 +1,57 @@
-/*
- * Jugador.h
- *
- *  Created on: Oct 18, 2017
- *      Author: lubuntu
- */
-
 #ifndef JUGADOR_H_
 #define JUGADOR_H_
-
 #include "Planilla.h"
+#include "Casilla.h"
+#include "Tablero.h"
 
 
+class Jugador{
 
-class Jugador {
+private:
 
-	private:
-		int numeroDeJugador;
-		bool sigueJugando;
-		Planilla PlanillaJugador(int numeroDeJugador);
+        bool estaJugando;
+
+        Planilla* planillaJugador;
+
+        unsigned int identificadorJugador;
+
+public:
+
+        /*
+        PRE: Es necesario un numero para identificar al jugador, pasado como parametro
+        POST: Crea un jugador con el identificador recibido
+        */
+        Jugador(unsigned int identificador);
+
+        /*
+        POST: Cambia getEstaJugando a falso (caso de que el jugador pierde)
+        */
+
+        void cambiarJugadorAPerdido();
+
+        /*
+        POST: Devuelve si el jugador esta jugando o no
+        */
+
+        bool getEstaJugando();
+
+        /*
+        POST: Devuelve el numero que identifica al jugador
+        */
+
+        unsigned int getIdentificador();
+
+        /*
+        POST: Realiza una jugada, haciendo que el usuario ingrese la posicion elegida, al
+        igual que el tipo de jugada a realizar. Interactua con tablero y casillero
+        */
+
+        void jugada(Tablero* tablero);
+
+        ~Jugador();
 
 
-
-	public:
-		//Pre: "jugadorNumero" debe ser mayor a 0.
-		//Post: Se crea el jugador en el ambito del juego.
-		Jugador(int jugadorNumero);
-		//Pre: La jugada debe ser: D para descubir casillero
-		//						   B para colocar bandera
-		//						   R para recolocar la bandera de otro jugador
-		//						   Q para quitar una bandera.
-		//						   S para que el jugador deje de jugar.
-		//Post: Se efectua la jugada solicitada.
-		void Jugada(char accion);
-		virtual ~Jugador();
 };
 
-#endif /* JUGADOR_H_ */
+
+#endif // JUGADOR_H_
