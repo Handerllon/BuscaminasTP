fcfc0d766935cfcac9357c76bd3c03054fe55884
benjaortiz <32141691+benjaortiz@users.noreply.github.com>
2017-10-28 16:19:35 -0300

    Update tablero.cpp

--- tablero.cpp
+++ tablero.cpp
@@ -8,11 +8,11 @@ Tablero::Tablero(unsigned int cantidadFilas, unsigned int cantidadColumnas){
 	for(unsigned int i=0; i<cantidadFilas; i++){
 		this->tablero[i]= new Casilla[cantidadColumnas];
 	}
-	Casilla actual;
+	Casilla* actual;
 	for(unsigned int i=0; i<cantidadFilas; i++){
 		for(unsigned int j=0; j<cantidadColumnas; j++){
-			actual=this->tablero[i][j];
-			actual.cambiarCoordenadas(i,j);
+			actual=this->obtenerCasillero(i,j);
+			actual->cambiarCoordenadas(i,j);
 		}
 	}
 }
@@ -28,14 +28,13 @@ unsigned int Tablero::obtenerCantidadColumnas(){
 }
 
 Casilla* Tablero::obtenerCasillero(unsigned int filaDeseada, unsigned int columnaDeseada){
-	Casilla* aObtener = &tablero[filaDeseada][columnaDeseada]
+	Casilla* aObtener = &tablero[filaDeseada][columnaDeseada];
 	return aObtener;
 }
 
 Tablero::~Tablero(){
 
 	unsigned int totalFilas=this->obtenerCantidadFilas();
-	unsigned int totalColumnas=this->obtenerCantidadColumnas();
 
 	for(unsigned int i=0; i<totalFilas; i++){
 
