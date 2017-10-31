3ab301a5f4a77ad76cd338669a84b0281fa4d0e9
benjaortiz <32141691+benjaortiz@users.noreply.github.com>
2017-10-31 01:05:06 -0300

    Update tablero.h

--- tablero.h
+++ tablero.h
@@ -33,11 +33,22 @@ public:
 	unsigned int obtenerCantidadColumnas();
 
 	/*
-	 * PRE: recibe la fila y columna de la casilla que se quiere obtener
+	 * PRE: recibe la fila y columna de la casilla que se quiere obtener, estos valores tienen que estar
+	 * 		entre 0 y obtenerCantidadFilas() / obtenerCantidadColumnas().
 	 * POST: devuelve la casilla de la posicion filaDeseada y columnaDeseada.
 	 */
 	Casilla* obtenerCasillero(unsigned int filaDeseada, unsigned int columnaDeseada);
-
+	
+	/*
+	 *POST:devuelve true si las coordenadas recibidas estan dentro del rango del tablero.
+	 */
+	bool coordenadasValidas(unsigned int fila, unsigned int columna);
+	
+	/*
+	PRE: Es necesario que el tablero este incicializado, ya con las minas puestas en cada una de las casillas
+	POST: Asigna las minas cercanas a cada uno de los casilleros dentro del tablero
+	*/
+	void calcularProximidades();
 
 	/*
 	 * Libera los recursos asociados a la clase.
