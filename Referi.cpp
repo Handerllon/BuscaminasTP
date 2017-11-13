647311c1b62152f843ce39bcdbcff9abbcd52eaa
benjaortiz <32141691+benjaortiz@users.noreply.github.com>
2017-11-13 13:55:19 -0300

    Update Referi.cpp

--- Referi.cpp
+++ Referi.cpp
@@ -6,29 +6,96 @@ Referi::Referi() {
 	this->cantJugadores = 2;
 	this->dificultad= MEDIA;
 	this->jugadores = new Lista<Jugador*>;
+	this->arrayJugadores=NULL;
 }
-	
+
 Referi::Referi(unsigned int cantJugadores, char dificultad) {
 	//jugadores es enviado por metodo de inicializador
 	this->cantJugadores=cantJugadores;
-	this->dificultad=dificultad;			
+	this->dificultad=dificultad;
 	this->jugadores = new Lista<Jugador*>;
-	
+
 	//Se crean los jugadores y se los agrega a la lista circular
-	Jugador* arrayJugadores = new Jugador[cantJugadores];
+	this->arrayJugadores = new Jugador[cantJugadores];
 	for (int i = 1; i <= cantJugadores; i++) {
 		arrayJugadores[i-1].setIdentificador(i);
 		jugadores->agregar(arrayJugadores + (i-1));
 	}
 }
-	
 
 
 Lista<Jugador*>* Referi::getJugadores() {
 	return jugadores;
 }
 
-Referi::~Referi() {
+bool Referi::hayJugadoresVivos(){
+	int jugadoresVivos=0;
 
+		this->getJugadores()->iniciarCursor();
+		while(this->getJugadores()->avanzarCursor()){
+
+			Jugador* jugadorActual = this->getJugadores()->obtenerCursor();
+			if(jugadorActual->getEstaJugando()){
+				jugadoresVivos++;
+			}
+
+		}
+		return jugadoresVivos > 0;
+}
+
+void Referi::ejecutarRonda(Graficador &buscaminas, Tablero* pTablero){
+	this->getJugadores()->iniciarCursor();
+
+	while((this->getJugadores()->avanzarCursor())) {
+		Jugador* jugadorDeTurno = this->getJugadores()->obtenerCursor();
+		if(jugadorDeTurno->getEstaJugando()){
+			std::cout << "Turno del jugador " << jugadorDeTurno->getIdentificador() << std::endl;
+			jugadorDeTurno->jugada(pTablero);
+			Casilla* casilla = pTablero->obtenerCasillero(jugadorDeTurno->obtenerCoordenadaYJugada(),
+								jugadorDeTurno->obtenerCoordenadaXJugada());
+
+			if(casilla->mostrarCasilla()== '0'){
+				for(int i=0; i < pTablero->obtenerCantidadFilas();i++){
+					for(int j=0; j < pTablero->obtenerCantidadColumnas();j++){
+						Casilla* casillaActual= pTablero->obtenerCasillero(i,j);
+						if(!casillaActual->estaOculta()){
+							buscaminas.imprimirJugada(i+1,j+1,casillaActual->mostrarCasilla());
+						}
+					}
+				}
+			}
+			else{
+				buscaminas.imprimirJugada(jugadorDeTurno->obtenerCoordenadaYJugada()+1,
+							  jugadorDeTurno->obtenerCoordenadaXJugada()+1,
+							  casilla->mostrarCasilla());
+			}
+
+			if (casilla->mostrarCasilla() == MINA) {
+				jugadorDeTurno->cambiarJugadorAPerdido();
+				std::cout << "Ha perdido el jugador " << jugadorDeTurno->getIdentificador() <<
+						" !" <<std::endl;
+			}
+		}
+	}
+}
+void Referi::mostrarPuntajes(){
+
+	std::cout<<"+++++++++++++++++++++++++++++"<<std::endl;
+	std::cout<<"PUNTAJES ACTUALES:"<<std::endl<<std::endl;
+
+	this->getJugadores()->iniciarCursor();
+
+	while(this->getJugadores()->avanzarCursor()){
+		Jugador* jugadorActual= this->getJugadores()->obtenerCursor();
+		std::cout<<"Jugador: "<<jugadorActual->getIdentificador()<<
+		std::cout<<":   "<<jugadorActual->getPuntajeJugador()<<"puntos";
+		std::cout<<std::endl;
+	}
+
+	std::cout<<"+++++++++++++++++++++++++++++"<<std::endl;
+}
+
+Referi::~Referi() {
+	delete[]arrayJugadores;
 	delete jugadores;
 }
