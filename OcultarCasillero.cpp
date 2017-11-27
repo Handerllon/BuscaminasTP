1c3d7274d84368b2f4f021c0b591351c4b675295
Handerllon <manuel121097@gmail.com>
2017-11-27 18:22:25 -0300

    Update OcultarCasillero.cpp

--- OcultarCasillero.cpp
+++ OcultarCasillero.cpp
@@ -8,25 +8,27 @@ void ocultarCasilleros(Tablero* tablero, unsigned int filaElegida, unsigned int 
 
 //Como es una jugada ya realizada, se le quitan las verificaciones de coordenadas validas
 void Algo::ocultarCasilleros(Tablero* tablero, unsigned int filaElegida, unsigned int columnaElegida){
+	if(tablero->coordenadasValidas(filaElegida,columnaElegida)){
+		Casilla* casillaElegida= tablero->obtenerCasillero(filaElegida, columnaElegida);
+		
+		//Ocultar Casillero seria un metodo de casilla, que lo cambia a oculto
+		casillaElegida->ocultarCasillero();
 
-	Casilla* casillaElegida= tablero->obtenerCasillero(filaElegida, columnaElegida);
-	//Ocultar Casillero seria un metodo de casilla, que lo cambia a oculto
-	casillaElegida->ocultarCasillero();
+		if(!casillaElegida->tieneMina()){
+			//Se cambio a un -1, ya que debe agregar casillas a casillas por descubrir
+			tablero->cambiarCasillasPorDescubrir(-1);
 
-	if(!casillaElegida->tieneMina()){
-		//Se cambio a un -1, ya que debe agregar casillas a casillas por descubrir
-		tablero->cambiarCasillasPorDescubrir(-1);
+			if(casillaElegida->getMinasCercanas()==0){
 
-		if(casillaElegida->getMinasCercanas()==0){
-
-			ocultarCasilleros(tablero,filaElegida+1,columnaElegida);
-			ocultarCasilleros(tablero,filaElegida-1,columnaElegida);
-			ocultarCasilleros(tablero,filaElegida,columnaElegida+1);
-			ocultarCasilleros(tablero,filaElegida,columnaElegida-1);
-			ocultarCasilleros(tablero,filaElegida+1,columnaElegida+1);
-			ocultarCasilleros(tablero,filaElegida+1,columnaElegida-1);
-			ocultarCasilleros(tablero,filaElegida-1,columnaElegida+1);
-			ocultarCasilleros(tablero,filaElegida-1,columnaElegida-1);
+				ocultarCasilleros(tablero,filaElegida+1,columnaElegida);
+				ocultarCasilleros(tablero,filaElegida-1,columnaElegida);
+				ocultarCasilleros(tablero,filaElegida,columnaElegida+1);
+				ocultarCasilleros(tablero,filaElegida,columnaElegida-1);
+				ocultarCasilleros(tablero,filaElegida+1,columnaElegida+1);
+				ocultarCasilleros(tablero,filaElegida+1,columnaElegida-1);
+				ocultarCasilleros(tablero,filaElegida-1,columnaElegida+1);
+				ocultarCasilleros(tablero,filaElegida-1,columnaElegida-1);
+			}
 		}
 	}
 }
