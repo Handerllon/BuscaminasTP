8ab43528355df7a5d24773312f0dfb314173a98f
Handerllon <manuel121097@gmail.com>
2017-11-29 12:45:51 -0300

    Mismo que cpp

--- Jugada.h
+++ Jugada.h
@@ -22,7 +22,7 @@ public:
 	//Pre: Debe recibir los datos de la jugada que ocurrio para ser creado
 	//Post: Crea la jugada con todos los datos de la jugada que fue realizada
 	Jugada(Casilla* casilla, int identificador, int opcion);
-
+/*
 	//Pre: 'filaJugada' debe estar entre 0 y tablero->ObtenerFilas()
 	//Post: Almacena la fila que se realizo en la jugada
 	void modificarFilaDeJugada(int filaJugada);
@@ -30,7 +30,7 @@ public:
 	//Pre: 'columnaJugada' debe estar entre 0 y tablero->ObtenerFilas()
 	//Post: Almacena la columna que se hizo en la jugada
 	void modificarColumnaDeJugada(int columnaJugada);
-
+*/
 	//Pre: 'jugadaDeJugadorNumero' debe estar entre 0 y referi->obtenerCantidadDeJugadores()
 	//Post: Modifica el numero del jugador que realizo la jugada
 	void modificarJugadorDeLaJugada(int jugadorNumero);
@@ -38,7 +38,7 @@ public:
 	//Pre: 'opcionElegida' debe ser o una bandera, o descubir un casillero, o quitar bandera
 	//Post: modifica la opcion de la jugada a un valor valido
 	void modificarOpcionElegidaParaJugar(char opcionElegida);
-
+/*
 	//Pre:El 'valorActual' debe estar entre 0 y 8, o Bandera('B')
 	//Post: Almacena el valor que posee la casilla moficada
 	void modificarElValorDeLaCasilla(char valorActual);
@@ -48,12 +48,15 @@ public:
 
 	//Post: devuelve el valor de la columna en la que se realizo la jugada
 	int getColumnaDescubierta();
-
+*/
 	//Post: devuelve el identificador del jugadorq ue realizo la jugada
 	int getJugadorDeLaJugada();
 
 	//Post: devuelve el tipo de jugada que decidio hacer el jugador
 	char getTipoDeJugada();
+	
+	//Post: devuelve la casilla donde se realizo la jugada
+	Casilla* getCasilla();
 
 
 	virtual ~Jugada();
